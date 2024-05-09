692fb5dac24e8d39057c3dd2c5126bc891329413
charan9999 <38738068+charan9999@users.noreply.github.com>
2024-05-09 07:49:34 +0400

    added router and seperated into pages for destructuring components

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 const HomeCards = () => {
 
     return (
-        <section className="py-4">
+        <div className="py-4">
             <div className="container-xl lg:container m-auto">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
                     <Card>
@@ -32,7 +32,7 @@ const HomeCards = () => {
                     </Card>
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 export default HomeCards
---
+++ src/components/ViewAllJobs.jsx
@@ -0,0 +1,12 @@
+const ViewAllJobs = () => {
+    return (
+        <div className="m-auto max-w-lg my-10 px-6">
+            <a
+                href="/jobs"
+                className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+            >View All Jobs</a
+            >
+        </div>
+    )
+}
+export default ViewAllJobs
